5f32fecb9bcb6ffb34b58c382ac59abb24743186
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-02 17:21:23 +0700

    Update server.js

--- src/server.js
+++ src/server.js
@@ -190,7 +190,7 @@ const sendPurchaseSummary = async (to, productId, quantity) => {
                         type: 'button',
                         button: {
                             type: 'url',
-                            url: 'https://example.com/rekening'
+                            url: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgAhW4uJgRKyuSGUry_tl9ThTvSQpqXSRXkP_Bho7kV8O_JXWDxosFR_mzyZvl7zb5MdRy-e6SPkgtWqiwByxH112KX51tzoCoANRy0O8FK_x8Mwsg5LanHS0qES9OeJI35bK27SIQ7013TyCImQVQLtsyqy-IyeSb5cIucM0Znpi1BsXPLEa4vr41m2BM/s682/qr.jpg'
                         }
                     },
                     {
