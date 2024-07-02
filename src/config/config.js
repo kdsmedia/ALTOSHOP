8c3065846cc4e91aeb620d21b7204894aa6cd933
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-02 17:15:18 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     whatsappApiToken: process.env.WHATSAPP_API_TOKEN,
     phoneNumberId: process.env.PHONE_NUMBER_ID,
-    qrCodeUrl: 'https://example.com/qr-code.png'  // Ganti dengan URL QR Code yang sesuai
+    qrCodeUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgAhW4uJgRKyuSGUry_tl9ThTvSQpqXSRXkP_Bho7kV8O_JXWDxosFR_mzyZvl7zb5MdRy-e6SPkgtWqiwByxH112KX51tzoCoANRy0O8FK_x8Mwsg5LanHS0qES9OeJI35bK27SIQ7013TyCImQVQLtsyqy-IyeSb5cIucM0Znpi1BsXPLEa4vr41m2BM/s682/qr.jpg'  // Ganti dengan URL QR Code yang sesuai
 };
