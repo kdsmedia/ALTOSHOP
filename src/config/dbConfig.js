c7047575c3fbc3a19da2b7b2a707e0bcc6f5d7ba
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-02 21:46:28 +0700

    Update dbConfig.js

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -3,21 +3,23 @@ const { parse } = require('url');
 
 const dbUrl = process.env.JAWSDB_URL;
 const { auth, hostname, port, pathname } = parse(dbUrl);
+const [user, password] = auth.split(':');
+const database = pathname.substring(1);
 
 const connection = mysql.createConnection({
-    host: hostname,
-    user: auth.split(':')[0],
-    password: auth.split(':')[1],
-    database: pathname.split('/')[1],
-    port: port
+  host: hostname,
+  user: user,
+  password: password,
+  database: database,
+  port: port
 });
 
 connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err.stack);
-        return;
-    }
-    console.log('Connected to the database as ID', connection.threadId);
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as ID', connection.threadId);
 });
 
 module.exports = connection;
