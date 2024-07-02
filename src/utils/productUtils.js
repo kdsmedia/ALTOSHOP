ee2218a188a1d9d9d7dfb6fad71701a984515f29
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-02 21:46:58 +0700

    Update productUtils.js

--- src/utils/productUtils.js
+++ src/utils/productUtils.js
@@ -1,25 +1,25 @@
 const db = require('../config/dbConfig');
 
 const getProductList = () => {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM products', (err, results) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(results);
-        });
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM products', (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results);
     });
+  });
 };
 
 const getProductById = (id) => {
-    return new Promise((resolve, reject) => {
-        db.query('SELECT * FROM products WHERE id = ?', [id], (err, results) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(results[0]);
-        });
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM products WHERE id = ?', [id], (err, results) => {
+      if (err) {
+        return reject(err);
+      }
+      resolve(results[0]);
     });
+  });
 };
 
 module.exports = { getProductList, getProductById };
