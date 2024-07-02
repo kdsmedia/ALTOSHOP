b777ed18a29f2a5ec52bd947c678aa8742e57446
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-02 16:58:03 +0700

    Create config.js

---
+++ src/config/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    whatsappApiToken: process.env.WHATSAPP_API_TOKEN,
+    phoneNumberId: process.env.PHONE_NUMBER_ID,
+    qrCodeUrl: 'https://example.com/qr-code.png'  // Ganti dengan URL QR Code yang sesuai
+};
