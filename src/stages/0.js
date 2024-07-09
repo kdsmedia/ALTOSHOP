60055725859ccca5f0fcc475ab3c90341443e1b9
SIDHANIE <137620314+kdsmedia@users.noreply.github.com>
2024-07-09 20:33:03 +0700

    Delete src directory

--- src/stages/0.js
+++
@@ -1,9 +0,0 @@
-import { storage } from '../storage.js';
-
-export const initialStage = {
-  exec({ from }) {
-    storage[from].stage = 1;
-
-    return '👋 Halo \n\nSaya ALTO, *Robot Canggih* MEDIA. \n* ada yang bisa saya bantu?* 🙋‍♂️ \n-----------------------------------\n1️⃣ - ```PESAN SESUATU``` \n2️⃣ - ```CEK PENGIRIMAN```\n0️⃣ - ```NGOBROL BERSAMA ALTO```';
-  },
-};
